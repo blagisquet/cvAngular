0d0145a8677216dcf416899f840c0367c1195c66
blagisquet <b.lagisquet@gmail.com>
2019-06-16 14:36:12 +0200

    added route home cv competences

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { CvComponent } from './cv/cv.component';
+import { CompetencesComponent } from './competences/competences.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent },
+  {path: 'home', component: HomeComponent },
+  {path: 'cv', component: CvComponent },
+  {path: 'compétences', component: CompetencesComponent }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CvComponent,
+    CompetencesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
